5df2df09698685595af149cb14635bec6a89b8d0
Dave G <>
2023-02-15 02:27:32 -0400

    adding dydns script

---
+++ dydns_d.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#You can start this script at startup by adding to crontab
+#@reboot sh /path/to/linuxdyndns.sh
+
+if [ -z "$1" ]; then
+  echo "Error: first argument must be host"
+  exit 1
+fi
+HOST=$1
+
+if [ -z "$3" ]; then
+  echo "Error: second argument must be domain"
+  exit 1
+fi
+DOMAIN=$2
+
+if [ -z "$3" ]; then
+  echo "Error: third argument must be password"
+  exit 1
+fi
+PASSWORD=$3
+
+if [ -z "$4" ]; then
+  echo "Error: fourth argument must be interval in minutes"
+  exit 1
+fi
+INTERVAL=$4
+
+dns_ip=$(dig $HOST.$DOMAIN +short) #Check the registered IP in DNS
+while true
+do
+        public_ip=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com|awk -F'"' '{print $2}') #Check Public IP
+        if [ "$public_ip" != "$dns_ip" ]
+        then
+                curl "https://dynamicdns.park-your-domain.com/update?host=$HOST&domain=$DOMAIN&password=$PASSWORD&ip=$public_ip"
+                sleep 300 #Allow the DNS server to update
+                dns_ip=$(dig $HOST.$DOMAIN +short) #Check the new IP and use new IP to compare to current IP
+        fi
+        sleep $(($INTERVAL*60))
+done
