ff957a043d8f7f0e2a0fcb015b1111bbb82dd44e
Dave G <>
2023-06-05 16:40:18 -0300

    mute asra archer figure

--- src/store/model/amazon-jp.ts
+++ src/store/model/amazon-jp.ts
@@ -56,12 +56,14 @@ export const AmazonJp: Store = {
       series: 'susanowo regalia',
       url: 'https://www.amazon.co.jp/-/en/dp/B0C5D5XRFR',
     },
+    /*
     {
       brand: 'kotobukiya',
       model: 'figure',
       series: 'md scale asra archer',
       url: 'https://www.amazon.co.jp/-/en/dp/B0BHL2HG19/',
     },
+    */
     {
       brand: 'kotobukiya',
       model: 'figure',
